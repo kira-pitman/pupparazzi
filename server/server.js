cbf1f019cbd3d87257f7e24a392195b5f35904e0
Kira Pitman <kirapitman@gmail.com>
2023-08-02 11:02:44 +1200

    display puppy pictures on root route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import * as Path from 'node:path'
 
 import express from 'express'
 import hbs from 'express-handlebars'
+import fs from 'node:fs/promises'
 
 const server = express()
 
@@ -18,8 +19,20 @@ server.set('views', Path.resolve('server/views'))
 
 // Your routes/router(s) should go here
 
-server.get('/', (req, res) => {
-  res.send('Pupparazzi')
+// server.get('/', (req, res) => {
+//   res.send('Pupparazzi')
+// })
+
+server.get('/', async (req, res) => {
+  try {
+    const puppies = await fs
+      .readFile(Path.resolve('server/data/data.json'), 'utf-8')
+      .then((data) => JSON.parse(data))
+    console.log(puppies)
+    res.render('home', puppies)
+  } catch (err) {
+    console.error(err.message)
+  }
 })
 
 export default server
